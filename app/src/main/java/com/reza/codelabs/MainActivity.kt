909a916ecf887bd099c04ed14f2643cc678787a2
Reza Agustiana <reza.agustiana@gmail.com>
2020-03-11 21:38:54 +0700

    9.1. Using NavigationUI with an Options menu

--- app/src/main/java/com/reza/codelabs/MainActivity.kt
+++ app/src/main/java/com/reza/codelabs/MainActivity.kt
@@ -2,11 +2,27 @@ package com.reza.codelabs
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.navigation.findNavController
+import androidx.navigation.ui.onNavDestinationSelected
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.overflow_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return item.onNavDestinationSelected(findNavController(R.id.nav_host_fragment))
+                || super.onOptionsItemSelected(item)
     }
 }
