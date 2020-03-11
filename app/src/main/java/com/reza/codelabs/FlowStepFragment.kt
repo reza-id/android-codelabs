0d97bc937cea6e6b97c590f362b8bb80096daba5
Reza Agustiana <reza.agustiana@gmail.com>
2020-03-11 18:53:32 +0700

    8. Add argument for destinations

--- app/src/main/java/com/reza/codelabs/FlowStepFragment.kt
+++ app/src/main/java/com/reza/codelabs/FlowStepFragment.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_flow_step_one.*
 
 class FlowStepFragment : Fragment() {
 
@@ -20,5 +22,13 @@ class FlowStepFragment : Fragment() {
         }
     }
 
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        bt_next?.setOnClickListener {
+            findNavController().navigate(R.id.action_step)
+        }
+    }
 
 }
