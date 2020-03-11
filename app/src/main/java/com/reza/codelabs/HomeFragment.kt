36ff92115936eb73ea92ce1d1898b731a3529985
Reza Agustiana <reza.agustiana@gmail.com>
2020-03-11 18:38:45 +0700

    6. Changing the Navigation Transition

--- app/src/main/java/com/reza/codelabs/HomeFragment.kt
+++ app/src/main/java/com/reza/codelabs/HomeFragment.kt
@@ -3,7 +3,8 @@ package com.reza.codelabs
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
-import androidx.navigation.Navigation
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.navOptions
 import kotlinx.android.synthetic.main.fragment_home.*
 
 class HomeFragment : Fragment(R.layout.fragment_home) {
@@ -11,10 +12,19 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val options = navOptions {
+            anim {
+                enter = R.anim.slide_in_right
+                exit = R.anim.slide_out_left
+                popEnter = R.anim.slide_in_left
+                popExit = R.anim.slide_out_right
+            }
+        }
+
         navigate_action_button.setOnClickListener {
             // findNavController().navigate(R.id.dest_step_one)
-            // findNavController().navigate(R.id.dest_step_one, null)
-            Navigation.createNavigateOnClickListener(R.id.dest_step_one, null)
+            findNavController().navigate(R.id.dest_step_one, null, options)
+            // Navigation.createNavigateOnClickListener(R.id.dest_step_one, null)
         }
     }
 }
