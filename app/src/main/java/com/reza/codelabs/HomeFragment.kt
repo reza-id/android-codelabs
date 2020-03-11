ac67bc99082f9fbda0d3f8bd9bff4b12c2136f61
Reza Agustiana <reza.agustiana@gmail.com>
2020-03-11 18:11:51 +0700

    5. Navigate to a Destination with NavController

--- app/src/main/java/com/reza/codelabs/HomeFragment.kt
+++ app/src/main/java/com/reza/codelabs/HomeFragment.kt
@@ -1,5 +1,20 @@
 package com.reza.codelabs
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_home.*
 
-class HomeFragment : Fragment(R.layout.fragment_home)
+class HomeFragment : Fragment(R.layout.fragment_home) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        navigate_action_button.setOnClickListener {
+            // findNavController().navigate(R.id.dest_step_one)
+            // findNavController().navigate(R.id.dest_step_one, null)
+            Navigation.createNavigateOnClickListener(R.id.dest_step_one, null)
+        }
+    }
+}
