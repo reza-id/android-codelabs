5990a9884c040905c752f1e9f6342889ba798a5e
Reza Agustiana <reza.agustiana@gmail.com>
2020-03-12 14:25:53 +0700

    Step 1: Add a fragment class

---
+++ app/src/main/java/com/reza/codelabs/BlankFragment.kt
@@ -0,0 +1,21 @@
+package com.reza.codelabs
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class BlankFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return null
+    }
+
+}
