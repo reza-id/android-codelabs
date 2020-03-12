87e8562d8da3112e38085ce36364e50283daf28d
Reza Agustiana <reza.agustiana@gmail.com>
2020-03-12 15:02:48 +0700

    6. Task: Add conditional navigation

--- app/src/main/java/com/reza/codelabs/GameFragment.kt
+++ app/src/main/java/com/reza/codelabs/GameFragment.kt
@@ -23,6 +23,7 @@ import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import com.reza.codelabs.databinding.FragmentGameBinding
 
 class GameFragment : Fragment() {
@@ -122,9 +123,11 @@ class GameFragment : Fragment() {
                         binding.invalidateAll()
                     } else {
                         // We've won!  Navigate to the gameWonFragment.
+                        view.findNavController().navigate(R.id.action_dest_game_to_gameWonFragment)
                     }
                 } else {
                     // Game over! A wrong answer sends us to the gameOverFragment.
+                    view.findNavController().navigate(R.id.action_dest_game_to_gameOverFragment)
                 }
             }
         }
