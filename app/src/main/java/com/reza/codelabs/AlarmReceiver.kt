d2afe168c1c692547c43338a3df1f1254d98d509
Reza Agustiana <reza.agustiana@gmail.com>
2020-03-26 20:08:55 +0700

    Create Alarm Receiver

---
+++ app/src/main/java/com/reza/codelabs/AlarmReceiver.kt
@@ -0,0 +1,35 @@
+package com.reza.codelabs
+
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import androidx.core.app.NotificationCompat
+
+class AlarmReceiver : BroadcastReceiver() {
+
+    private lateinit var mNotificationManager: NotificationManager
+
+    override fun onReceive(context: Context, intent: Intent) {
+        mNotificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        deliverNotification(context)
+    }
+
+
+    private fun deliverNotification(context: Context) {
+        val contentIntent = Intent(context, MainActivity::class.java)
+        val contentPendingIntent = PendingIntent.getActivity(context, NOTIFICATION_ID, contentIntent, PendingIntent.FLAG_UPDATE_CURRENT)
+
+        val builder = NotificationCompat.Builder(context, CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic_stars)
+            .setContentTitle("Stand Up Alert")
+            .setContentText("You should stand up and walk around now!")
+            .setContentIntent(contentPendingIntent)
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setAutoCancel(true)
+            .setDefaults(NotificationCompat.DEFAULT_ALL)
+
+        mNotificationManager.notify(NOTIFICATION_ID, builder.build())
+    }
+}
--- app/src/main/java/com/reza/codelabs/MainActivity.kt
+++ app/src/main/java/com/reza/codelabs/MainActivity.kt
@@ -1,13 +1,9 @@
 package com.reza.codelabs
 
 import android.app.NotificationManager
-import android.app.PendingIntent
-import android.content.Context
-import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.NotificationCompat
 import kotlinx.android.synthetic.main.activity_main.*
 
 
@@ -24,7 +20,6 @@ class MainActivity : AppCompatActivity() {
         alarmToggle.setOnCheckedChangeListener { buttonView, isChecked ->
             var toastMessage = ""
             toastMessage = if (isChecked) {
-                deliverNotification(this)
                 "Hurung!!!"
             } else {
                 mNotificationManager.cancelAll()
@@ -33,21 +28,5 @@ class MainActivity : AppCompatActivity() {
 
             Toast.makeText(this, toastMessage, Toast.LENGTH_SHORT).show()
         }
-    }
-
-    private fun deliverNotification(context: Context) {
-        val contentIntent = Intent(context, MainActivity::class.java)
-        val contentPendingIntent = PendingIntent.getActivity(context, NOTIFICATION_ID, contentIntent, PendingIntent.FLAG_UPDATE_CURRENT)
-
-        val builder = NotificationCompat.Builder(context, CHANNEL_ID)
-            .setSmallIcon(R.drawable.ic_stars)
-            .setContentTitle("Stand Up Alert")
-            .setContentText("You should stand up and walk around now!")
-            .setContentIntent(contentPendingIntent)
-            .setPriority(NotificationCompat.PRIORITY_HIGH)
-            .setAutoCancel(true)
-            .setDefaults(NotificationCompat.DEFAULT_ALL)
-
-        mNotificationManager.notify(NOTIFICATION_ID, builder.build())
     }
 }
