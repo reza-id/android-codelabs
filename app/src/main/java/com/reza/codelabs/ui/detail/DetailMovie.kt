360c61fb70e2feb90c7bffbd57807539dbdb34d5
Reza Agustiana <reza.agustiana@gmail.com>
2020-04-18 17:51:26 +0700

    UI and NetworkState

---
+++ app/src/main/java/com/reza/codelabs/ui/detail/DetailMovie.kt
@@ -0,0 +1,13 @@
+package com.reza.codelabs.ui.detail
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.reza.codelabs.R
+
+class DetailMovie : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_movie)
+    }
+}
