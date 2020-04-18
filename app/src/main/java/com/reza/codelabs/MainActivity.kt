197ec51e9de866b3b96e36623292057a26987e8b
Reza Agustiana <reza.agustiana@gmail.com>
2020-04-18 13:05:39 +0700

    Event Listener

--- app/src/main/java/com/reza/codelabs/MainActivity.kt
+++ app/src/main/java/com/reza/codelabs/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.reza.codelabs
 
+import android.content.Context
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.reza.codelabs.databinding.ActivityMainBinding
@@ -8,6 +11,7 @@ import com.reza.codelabs.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var handler: MainActivityClickHandler
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,5 +20,19 @@ class MainActivity : AppCompatActivity() {
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
         binding.student = Student("reza", "agustiana@reza.web.id")
+
+        handler = MainActivityClickHandler(this)
+        binding.clickHandler = handler
+    }
+
+    inner class MainActivityClickHandler(val context: Context) {
+
+        fun onEnrollButtonClicked(view: View) {
+            Toast.makeText(context, "Enroll Clicked", Toast.LENGTH_LONG).show()
+        }
+
+        fun onCancelButtonClicked(view: View) {
+            Toast.makeText(context, "Cancel Clicked", Toast.LENGTH_LONG).show()
+        }
     }
 }
