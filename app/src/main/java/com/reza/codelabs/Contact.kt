d2108c6cea4f5681f4771846f550b8b5952101cc
Reza Agustiana <reza.agustiana@gmail.com>
2020-04-18 14:05:28 +0700

    two way data binding

--- app/src/main/java/com/reza/codelabs/Contact.kt
+++ app/src/main/java/com/reza/codelabs/Contact.kt
@@ -1,3 +1,17 @@
 package com.reza.codelabs
 
-data class Contact(val id: Int, var name: String, var phone: String)
+import androidx.databinding.BaseObservable
+import androidx.databinding.Bindable
+
+class Contact(
+    val id: Int,
+    name: String,
+    var phone: String
+) : BaseObservable() {
+    @Bindable
+    var name = name
+        set(value) {
+            field = value
+            notifyPropertyChanged(BR.name)
+        }
+}
