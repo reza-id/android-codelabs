6e5b0485df0f0643d61fc318cc1ef8d04e83861e
Reza Agustiana <reza.agustiana@gmail.com>
2020-05-14 15:09:14 +0700

    Add a content intent

--- app/src/main/java/com/reza/codelabs/utils/NotificationUtils.kt
+++ app/src/main/java/com/reza/codelabs/utils/NotificationUtils.kt
@@ -1,8 +1,11 @@
 package com.reza.codelabs.utils
 
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
 import androidx.core.app.NotificationCompat
+import com.reza.codelabs.MainActivity
 import com.reza.codelabs.R
 
 // Notification ID.
@@ -19,6 +22,16 @@ private const val FLAGS = 0
  */
 fun NotificationManager.sendNotification(messageBody: String, context: Context) {
 
+    // TODO: Step 1.11 create intent
+    val contentIntent = Intent(context, MainActivity::class.java)
+    // TODO: Step 1.12 create PendingIntent
+    val contentPendingIntent = PendingIntent.getActivity(
+        context,
+        NOTIFICATION_ID,
+        contentIntent,
+        PendingIntent.FLAG_UPDATE_CURRENT
+    )
+
     // TODO: Step 1.2 get an instance of NotificationCompat.Builder
     val builder = NotificationCompat.Builder(context, context.getString(R.string.egg_notification_channel_id))
         // TODO: Step 1.8 verify the notification channel name
@@ -27,6 +40,9 @@ fun NotificationManager.sendNotification(messageBody: String, context: Context) 
         .setSmallIcon(R.drawable.cooked_egg)
         .setContentTitle(context.getString(R.string.notification_title))
         .setContentText(messageBody)
+        // TODO: Step 1.13 set content intent
+        .setContentIntent(contentPendingIntent)
+        .setAutoCancel(true)
 
     // TODO: Step 1.4 call notify
     notify(NOTIFICATION_ID, builder.build())
